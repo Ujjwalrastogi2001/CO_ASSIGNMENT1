21e5fb3bac09012c39e4bd35ea788e17882e8d36
Mohit Sharma <sharmamohit78444@gmail.com>
2021-08-06 23:00:44 +0530

    base1

---
+++ Simple-Assembler/assembler/functions.py
@@ -0,0 +1,22 @@
+def getopcode(opcode):
+    t = {"add":"00000","sub":"00001",}
+    return t[opcode]
+    pass
+
+def getregcode():
+    pass
+
+def add():
+    r1 and r2
+    number whole number 
+    # add r1 
+    
+    pass
+
+def sub():
+    pass
+
+
+def store():
+    pass
+
---
+++ Simple-Assembler/assembler/main.py
@@ -0,0 +1,44 @@
+import functions as f
+
+
+
+def mainfun():
+    code = []     
+    overflow = False 
+    count = 0     #instructions stored
+    while(True):
+        try:
+            st = input()    
+            count = count+1        #user input 
+            if(count > 256):
+                overflow = True
+
+            st = st.split(' ')      # add R1 R2
+            string = ''                # binary
+            code.append(st)
+        except EOFError:
+            break
+    
+    n = 0
+    while(len(code) != n):
+        #label check 
+        #var
+        #halt
+        #total instruc < 256
+        pass
+
+    while(len(code) != n):
+        
+        #function 
+        op = f.getopcode(st[1])
+        string = string + f.getopcode(st[0])   #opcode
+        string = string + f.getregcode()
+        if( op == 00000):
+            add(st[1],st[2])
+
+        
+ 
+
+mainfun()
+
+
